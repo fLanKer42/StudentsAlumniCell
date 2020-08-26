f468015024fd6c8008440eb9ca637e60d36c11c9
fLanKer42 <dhruv.rai2001@gmail.com>
2020-08-26 21:10:16 +0530

    Document object model files added

---
+++ myscripts.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var name, review;
+    name = document.getElementById("Name").value;
+    review = document.getElementById("Review").value;
+    if (name === ""||review === "") {
+        console.log("hh");
+        document.getElementById("error").innerHTML = "Please fill up the form correctly";
+        document.getElementById("error").style.display = "block";
+    }
+    else {
+        temp = document.getElementById("table").innerHTML;
+        document.getElementById("table").innerHTML = temp + "<tr><td>" + name + "</td><td>" + review + "</td></tr>";
+        document.getElementById("error").style.display = "none";
+    }
+}
