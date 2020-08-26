f77b4fc58589f902ebe8292115449e34a41d7d6e
fLanKer42 <dhruv.rai2001@gmail.com>
2020-08-27 00:53:42 +0530

    replacing -1 with NOT POSSIBLE string

--- Q3_generalProgramming2_carFueling.py
+++ Q3_generalProgramming2_carFueling.py
@@ -5,22 +5,21 @@ def compute_min_refills(distance, tank, stops):
     for i in range(length):
         if stops[i]>(tank+pre_stop):
             if (stops[i-1]-pre_stop)>tank:
-                return -1
+                return "NOT POSSIBLE"
             pre_stop = stops[i-1]
             return_val += 1
     if distance>(tank+pre_stop):
         
         if (stops[length-1] - stops[length-2])>tank:
-            return -1
+            return "NOT POSSIBLE"
         if distance>(tank+stops[length-1]):
-            return -1
+            return -"NOT POSSIBLE"
         return_val += 1
             
     return return_val
 
 if __name__ == '__main__':
-    d = int(input())
-    m = int(input())
-    n = int(input())
+    d = int(input()) #distance of destination
+    m = int(input()) #distance on full tank
     stops = list(map(int, input().split()))
     print(compute_min_refills(d, m, stops))
